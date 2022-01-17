5ac9465f6fd2af5b1b75ca37ca5114c4c5063f7d
Daniel Soares <danielpsc1993@gmail.com>
2022-01-17 12:15:51 -0300

    em desenvolvimento

---
+++ estrutura.js
@@ -0,0 +1,27 @@
+btnClose.addEventListener("click",() => {
+    imgGrande.style.display = "none";
+})
+
+
+img1.addEventListener("click",() => {
+    imgGrande.style.display = "block";
+    imgGrande.style.backgroundImage = "url(../onlineGym/imagens/local.png)";
+})
+
+
+img2.addEventListener("click",() => {
+imgGrande.style.display = "block";
+imgGrande.style.backgroundImage = "url(../onlineGym/imagens/local.png)";
+})
+
+
+img3.addEventListener("click",() => {
+imgGrande.style.display = "block";
+imgGrande.style.backgroundImage = "url(../onlineGym/imagens/local.png)";
+})
+
+
+img4.addEventListener("click",() => {
+imgGrande.style.display = "block";
+imgGrande.style.backgroundImage = "url(../onlineGym/imagens/local.png)";
+})
--- home.js
+++ home.js
@@ -35,33 +35,7 @@ btn.addEventListener("click",() => {
     }
 })
 
-    btnClose.addEventListener("click",() => {
-        imgGrande.style.display = "none";
-    })
-    
-    
-    img1.addEventListener("click",() => {
-        imgGrande.style.display = "block";
-        imgGrande.style.backgroundImage = "url(../onlineGym/imagens/local.png)";
-    })
-
-
-img2.addEventListener("click",() => {
-    imgGrande.style.display = "block";
-    imgGrande.style.backgroundImage = "url(../onlineGym/imagens/local.png)";
-})
-
-
-img3.addEventListener("click",() => {
-    imgGrande.style.display = "block";
-    imgGrande.style.backgroundImage = "url(../onlineGym/imagens/local.png)";
-})
-
-
-img4.addEventListener("click",() => {
-    imgGrande.style.display = "block";
-    imgGrande.style.backgroundImage = "url(../onlineGym/imagens/local.png)";
-})
+   
 
 
 
