243e013a5abd7cb3507557f722ddc8173fc3c2b7
Daniel Soares <danielpsc1993@gmail.com>
2022-02-06 11:49:15 -0300

    desenvolvendo

--- estrutura.js
+++ estrutura.js
@@ -2,6 +2,18 @@ var area1 = document.getElementById("estImg1");
 var area2 = document.getElementById("estImg2");
 var area3 = document.getElementById("estImg3");
 var area4 = document.getElementById("estImg4");
+var area5 = document.getElementById("estImg5");
+var area6 = document.getElementById("estImg6");
+var area7 = document.getElementById("estImg7");
+var area8 = document.getElementById("estImg8");
+var area9 = document.getElementById("estImg9");
+var area10 = document.getElementById("estImg10");
+var area11 = document.getElementById("estImg11");
+var area12 = document.getElementById("estImg12");
+var area13 = document.getElementById("estImg13");
+var area14 = document.getElementById("estImg14");
+var area15 = document.getElementById("estImg15");
+
 var estruturaFotos  = document.getElementById("estruturaFotos");
 var btnClose = document.getElementById("close")
 var btnLeft = document.getElementById("left");
@@ -49,8 +61,65 @@ imgGrande.style.backgroundImage = "url(../onlineGym/imagens/img4.jpg)";
 
 area4.addEventListener("click",() => {
 imgGrande.style.display = "block";
-imgGrande.style.backgroundImage = "url(../onlineGym/imagens/local.png)";
+imgGrande.style.backgroundImage = "url(../onlineGym/imagens/espaco6.jpg)";
 })
+
+area5.addEventListener("click",() => {
+    imgGrande.style.display = "block";
+    imgGrande.style.backgroundImage = "url(../onlineGym/imagens/img1.jpg)";
+})
+
+area6.addEventListener("click",() => {
+    imgGrande.style.display = "block";
+    imgGrande.style.backgroundImage = "url(../onlineGym/imagens/img2.jpg)";
+})
+
+area7.addEventListener("click",() => {
+    imgGrande.style.display = "block";
+    imgGrande.style.backgroundImage = "../onlineGym/imagens/img3.jpg)";
+})
+
+area8.addEventListener("click",() => {
+    imgGrande.style.display = "block";
+    imgGrande.style.backgroundImage = "url(../onlineGym/imagens/img4.jpg)";
+})
+
+area9.addEventListener("click",() => {
+    imgGrande.style.display = "block";
+    imgGrande.style.backgroundImage = "url(../onlineGym/imagens/img5.jpg)";
+})
+
+area10.addEventListener("click",() => {
+    imgGrande.style.display = "block";
+    imgGrande.style.backgroundImage = "url(../onlineGym/imagens/img6.jpg)";
+})
+
+area11.addEventListener("click",() => {
+    imgGrande.style.display = "block";
+    imgGrande.style.backgroundImage = "url(../onlineGym/imagens/img7.jpg)";
+})
+
+area12.addEventListener("click",() => {
+    imgGrande.style.display = "block";
+    imgGrande.style.backgroundImage = "url(../onlineGym/imagens/img8.jpg)";
+})
+
+area13.addEventListener("click",() => {
+    imgGrande.style.display = "block";
+    imgGrande.style.backgroundImage = "url(../onlineGym/imagens/img9.jpg)";
+})
+
+area14.addEventListener("click",() => {
+    imgGrande.style.display = "block";
+    imgGrande.style.backgroundImage = "url(../onlineGym/imagens/img13.jpg)";
+})
+
+area15.addEventListener("click",() => {
+    imgGrande.style.display = "block";
+    imgGrande.style.backgroundImage = "url(../onlineGym/imagens/img16.jpg)";
+})
+
+
 
 btnRight.addEventListener("click",() => {
     if(i<=18){
